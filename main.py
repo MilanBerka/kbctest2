cff711f4084dbb4c3882d08437fe2dc51ce5f6b7
MilanBerka <berkamilanml@gmail.com>
2017-03-21 11:17:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,16 +6,35 @@ Created on Tue Mar 21 10:34:14 2017
 """
 
 #gittest
+import pip
+pip.main(['install', '--disable-pip-version-check', '--no-cache-dir', 'pydrive'])
+pip.main(['install', '--disable-pip-version-check', '--no-cache-dir', 'io'])
+pip.main(['install', '--disable-pip-version-check', '--no-cache-dir', 'zipfile'])
+pip.main(['install', '--disable-pip-version-check', '--no-cache-dir', 'xml'])
+pip.main(['install', '--disable-pip-version-check', '--no-cache-dir', 'glob'])
+
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from io import BytesIO
+import zipfile 
+import pandas as pd
+import xml.etree.ElementTree as ET
+import glob  
 import pandas as pd
 from keboola import docker
 
 cfg = docker.Config()
 parameters = cfg.get_parameters()
 colName = parameters.get('colName')
-if type(colName) is str:
-  pass
-else:
-  colName = str(colName)
+import pandas as pd
+import numpy as np
 
-testFrame = pd.DataFrame({'id':[1,2,3],colName:[12,50,13]})
+  
+
+
+
+testFrame = pd.DataFrame({'id':[1,2,3],'value':[12,50,13]})
 testFrame.to_csv('/data/out/tables/result.csv',index=None)
+
+if 'a' is str:
+    print('ahoj')
