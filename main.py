0115a1fb1ddf12c71007cf2ff172f05411671891
MilanBerka <berkamilanml@gmail.com>
2017-03-21 11:05:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,5 +7,15 @@ Created on Tue Mar 21 10:34:14 2017
 
 #gittest
 import pandas as pd
-testFrame = pd.DataFrame({'id':[1,2,3],'value':[12,50,13]})
+from keboola import docker
+
+cfg = docker.Config()
+parameters = cfg.get_parameters()
+colName = parameters.get('colName')
+if colName is string:
+  pass
+else:
+  colName = str(colName)
+
+testFrame = pd.DataFrame({'id':[1,2,3],colName:[12,50,13]})
 testFrame.to_csv('/data/out/tables/result.csv',index=None)
