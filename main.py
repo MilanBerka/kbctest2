7f329a2be6d69aeb29f9c74001071e0f8a6bba6c
MilanBerka <berkamilanml@gmail.com>
2017-04-04 14:57:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -28,6 +28,9 @@ import json
 
 testFrame = pd.DataFrame({'id':[1,2,3],colName:[12,50,13]})
 testFrame.to_csv('/data/out/tables/result.csv',index=None)
-with open('/data/in/files/253414451_settings.yaml') as json_data:
-    print(json_data)
+with open('/data/in/files/253414451_settings.yaml') as shiiit:
+    print(shiiit.readlines())
+with open('/data/in/files/253410708_credentials.dat') as shiiit2:
+    print(shiiit2.readlines())
 
+    
