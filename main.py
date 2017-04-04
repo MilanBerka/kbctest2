1f2464e4ff7e24510696c2fc143a952a6628506e
MilanBerka <berkamilanml@gmail.com>
2017-04-04 15:22:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -29,16 +29,16 @@ import json
 testFrame = pd.DataFrame({'id':[1,2,3],colName:[12,50,13]})
 testFrame.to_csv('/data/out/tables/result.csv',index=None)
 if __name__ == '__main__':
-    with open('/data/in/files/253424893_settings.yaml') as shiiit:
+    with open('/data/in/files/253425786_settings.yaml') as shiiit:
         print(shiiit.readlines())
     with open('/data/in/files/253410708_credentials.dat') as shiiit2:
         print(shiiit2.readlines())
-    gauth = GoogleAuth(settings_file='/data/in/files/253424893_settings.yaml')
+    gauth = GoogleAuth(settings_file='/data/in/files/253425786_settings.yaml')
     #    gauth.LocalWebserverAuth() # Creates local webserver and auto handles authentication.
     drive = GoogleDrive(gauth)
     folderToLookAt = 'CSOB AM 2016'
     driveFilesList = drive.ListFile({'q':"mimeType='application/vnd.google-apps.folder' and title='{}' and trashed=false".format(folderToLookAt)}).GetList()                        
-    print(folderId = driveFilesList[0]['id'])
+    print(driveFilesList[0]['id'])
     
 
     
